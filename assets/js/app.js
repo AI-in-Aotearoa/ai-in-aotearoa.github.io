1595ee5e69b451b7e8bafe5a07ba97a77d558c72
Drew Broadley <drew@broadley.org.nz>
2023-10-25 13:15:50 +1300

    Initial look/feel up, and prompts tab loaded

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+
+  function togglePrompts() {
+
+    var promptsSection = document.getElementById('prompts');
+
+    if (promptsSection.style.display === "none")
+    {
+      promptsSection.style.display = "block";
+    } else {
+      promptsSection.style.display = "none";
+    }
+  }
